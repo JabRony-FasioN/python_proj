c113bfb5284280ed930da6851d20986041223112
JabRony-FasioN <81860042+JabRony-FasioN@users.noreply.github.com>
2024-04-23 16:28:30 +0300

    Create Cpu.py

---
+++ Cpu.py
@@ -0,0 +1,16 @@
+import psutil
+
+# Function to get current memory usage
+def get_memory_usage():
+    mem = psutil.virtual_memory()
+    return mem.percent
+
+
+# Set the memory threshold for the alarm
+memory_threshold = 50
+
+# Check memory usage and send alert if threshold is exceeded
+if get_memory_usage() > memory_threshold:
+    subject = 'Memory Usage Alert'
+    body = 'Memory usage is above {}%'.format(memory_threshold)
+    print(subject, body)
