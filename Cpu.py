c9ff34d3ec3e1128bfdfef48634b2cf25c0cc49a
JabRony-FasioN <81860042+JabRony-FasioN@users.noreply.github.com>
2024-04-24 16:46:18 +0300

    Update Cpu.py

--- Cpu.py
+++ Cpu.py
@@ -12,5 +12,6 @@ memory_threshold = 50
 # Check memory usage and send alert if threshold is exceeded
 if get_memory_usage() > memory_threshold:
     subject = 'Memory Usage Alert'
+    print("test")
     body = 'Memory usage is above {}%'.format(memory_threshold)
     print(subject, body)
